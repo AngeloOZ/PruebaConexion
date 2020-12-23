d5c6debb3c25cd658beb63293b3c8392e487966f
Angello Ordonez <angelo-mjz7@hotmail.com>
2020-12-22 21:44:23 -0500

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PruebaConexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string datos = "";
+            string CadenaConex = "datasource=db4free.net;port=3306;username=prueba_root; password=milena2000;database=mysql;";
+
+            MySqlConnection conexionDB = new MySqlConnection(CadenaConex);
+
+            try
+            {
+                conexionDB.Open();
+                
+                MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES", conexionDB);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    datos += reader.GetString(0)+"\n";
+                }
+                label1.Text = datos;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
